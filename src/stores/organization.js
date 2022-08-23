646ca5b191f1f231f6fab39281e16653ba33fb78
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2022-08-23 16:50:00 -0500

    Attempt 3 to not crash the browser

--- src/stores/organization.js
+++ src/stores/organization.js
@@ -283,7 +283,7 @@ export const useOrganizationStore = defineStore('organization', {
 						const upload = UpChunk.createUpload({
 							endpoint: res.data.url,
 							file: video,
-							chunkSize: 102400, // Uploads the file in ~30 MB chunks
+							chunkSize: 25600, // Uploads the file in ~30 MB chunks
 						});
 
 						upload.on('error', err => {
